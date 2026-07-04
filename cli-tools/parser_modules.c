fc5c68e04ae11fe6e6d87147e38aba41070d3b42
mks-mk <235512228+mks-mk@users.noreply.github.com.>
2026-07-04 11:45:56 +0530

    Improve question parser with dynamic memory allocation

--- cli-tools/parser_modules.c
+++ cli-tools/parser_modules.c
@@ -9,6 +9,7 @@
 #include "parser.h" //link with parser.h file 
 
 
+
 //function for removing unwanted spaces and tabs from a string
 char *trim_spaces(char *str)
 {
@@ -40,7 +41,9 @@ char *trim_spaces(char *str)
 
 
 
-//an helper function for 'parse_questions' function
+
+
+//an helper function for 'parse_questions' function, checking if the qn is a sub qn or not
 int is_sub_question(const char *str)
 {
     //move a temperory pointer to the past of any blank space or a tab
@@ -63,56 +66,155 @@ int is_sub_question(const char *str)
 
 
 
+
+
         
 
 //function for extracting only the exam Qns from a messy text file and turn them into a clean JSON list
 void parse_questions(FILE *input_file, FILE *output_file)
 {
-    char line_buffer[512]; //512 bytes is the safe here
-    int is_first_item = 1;
-    size_t len=0;
-    
-    //write the structure of JSON array header
-    fprintf(output_file, "{\n \"type\": \"questions\",\n \"list\": [\n");
-    
-    while(fgets(line_buffer, sizeof(line_buffer), input_file) != NULL)
-    {
-        if( strstr(line_buffer, "Q.") != NULL || strstr(line_buffer, "Question") != NULL || is_sub_question(line_buffer))
-        {
-            len = strlen(line_buffer);
-            if(len > 0 && line_buffer[len -1] == '\n')
-            {
-                line_buffer[len -1] = '\0';
-            }
-            if(!is_first_item)
-            {
-                fprintf(output_file, ",\n");
-            }
-            
-            fprintf(output_file, "   \"%s\"", trim_spaces(line_buffer)); 
+    char *single_line ;  
+    int max_line_size = 512;
+    int is_the_very_first_qn = 1;      //for adding commas in json structure, bcs the elements are seperated by commas in a json structure
+    int current_line_size=0;
 
-            is_first_item = 0;
+
+    char *full_question_texts;
+    int max_question_size = 2026;
+    int full_question_length = 0;
+
+    single_line = (char *)calloc(max_line_size , sizeof(char));      
+    full_question_texts = (char *)calloc(max_question_size , sizeof(char));
+
+    if(single_line == NULL || full_question_texts == NULL)
+    {
+        printf("System Error, Failed to allocate memory !\n");
+        exit(1);
+    }
+    
+    fprintf(output_file, "{\n \"type\": \"questions\",\n \"list\": [\n");  //writing the structure of JSON array header to the output file
+
+    //read line by lines from the input file and store in allocated memory single_line
+    while(fgets(single_line, max_line_size, input_file) != NULL)
+    {
+
+        current_line_size = strlen(single_line);
+        
+        while( current_line_size > 0 && single_line[current_line_size - 1] != '\n')
+        {
+            max_line_size *=2;
+            char *resized_line;
+            resized_line = realloc(single_line, max_line_size * sizeof(char));
+            if(resized_line == NULL)
+            {
+                printf("SySystem Error, Failed to allocate memory\n");
+                exit(1);
+            }
+            single_line = resized_line;
+
+            //update fgets() to avoid overwrite
+            if(fgets(single_line + current_line_size, max_line_size - current_line_size, input_file) == NULL)
+            {
+                break;
+            }
+            current_line_size = strlen(single_line);         
         }
+        
+        if( current_line_size > 0 && single_line[current_line_size - 1] == '\n')
+        {
+
+            single_line[current_line_size-1]='\0';
+        }
+        char *trimmed_question = trim_spaces(single_line);
+        if(strlen(trimmed_question) == 0 )   //
+        {
+            continue;
+        }
+
+        if( strstr(trimmed_question, "Question.") || strstr(trimmed_question, "Q.") || is_sub_question(trimmed_question))
+        {
+            if(full_question_length > 0)  //checking if there is any qns exists on buffer, if yes, then first print it on the output file in correct way, bcz that's completed.
+            {
+                if(!is_the_very_first_qn)  // is_the_very_first_qn != 1
+                {                          // ie, 1 != 1
+                    fprintf(output_file, ",\n");
+                }
+            
+                
+                    fprintf(output_file, "  \"%s\"", full_question_texts);
+                    is_the_very_first_qn = 0;
+                
+            }
+            //clear the buffer
+            strcpy(full_question_texts, trimmed_question);
+            full_question_length = strlen(full_question_texts);
+        }
+        else
+        {
+            if( full_question_length > 0)
+            {
+                if(full_question_length + strlen(trimmed_question) + 2 >= max_question_size)  // +2 = +1 for the space " ", +1 for the null terminator '\0'
+                {
+                    max_question_size *=2;
+                    char *resized_question = realloc( full_question_texts, max_question_size * sizeof(char));
+                    if( resized_question == NULL)
+                    {
+                        printf("System Error, Failed to allocate memory\n");
+                        exit(1);
+                    }
+
+                    full_question_texts = resized_question;
+                }
+
+                //continuation of the current question
+                strcat(full_question_texts, " ");
+                strcat(full_question_texts, trimmed_question);
+                full_question_length = strlen(full_question_texts);
+                
+            }
+        }
+        
     }
 
-    //close the JSON array formatting
-    fprintf(output_file, "\n  ]\n}\n");
-    printf("░░░ Question Bank JSON generated succesfully ! ░░░\n");
+    //print the final question remaining in the buffer
+    if( full_question_length > 0 )   // Buffer contains the last question?
+    {
+        if(!is_the_very_first_qn)     // Already printed previous questions?
+        {
+            fprintf(output_file, ",\n");
+        }
+
+        fprintf(output_file, "  \"%s\"", full_question_texts);
+    }
+
+
+    fprintf(output_file, "\n ]\n}\n");
+    
+    free(single_line);
+    free(full_question_texts);
+
+    printf("░░░ Question Bank JSON generated successfully! ░░░\n");
+
 }
+
+
+
+
+
 
 
 
 //function to take a raw messy syllabus text file and organize it into a structured JSON dictionary
 void parse_syllabus(FILE *input_file, FILE *output_file)
 {
-    char line_buffer[256];
-    int is_first_item = 1;
-    char current_module[64]="";
-    char *topic_accumulator;
-    size_t len = 0;
+    char line[256];
+    int is_first_note = 1;
+    char module_name[64]="";
+    char *module_topics;
+    int line_length = 0;
 
-    topic_accumulator =(char *)calloc(4096, sizeof(char));
-    if( topic_accumulator == NULL)
+    module_topics =(char *)calloc(4096, sizeof(char));
+    if( module_topics == NULL)
     {
         printf("System Error, Failed to allocate memory !\n");
         exit(1);
@@ -121,64 +223,65 @@ void parse_syllabus(FILE *input_file, FILE *output_file)
     //write the structure of JSON array header
     fprintf(output_file, "{\n  \"type\": \"syllabus\",\n  \"data\": {\n");
     
-    while(fgets(line_buffer, sizeof(line_buffer), input_file) != NULL)
+    while(fgets(line, sizeof(line), input_file) != NULL)
     {
-        if(strstr(line_buffer, "Module") != NULL || strstr(line_buffer, "module") != NULL)
+        if(strstr(line, "Module") != NULL || strstr(line, "module") != NULL)
         {
-            if (strlen(current_module) > 0)
+            if (strlen(module_name) > 0)
             {
-                if(!is_first_item)
+                if(!is_first_note)
                 {
                     fprintf(output_file, ",\n");
                 }
-                fprintf(output_file, "    \"%s\": \"%s\"", trim_spaces(current_module), trim_spaces(topic_accumulator));
-                is_first_item = 0;
+            fprintf(output_file, "    \"%s\": \"%s\"", trim_spaces(module_name), trim_spaces(module_topics));
+            is_first_note = 0;
             }
-            char *module_token, *topics_token;
+            char *module_title, *module_description;
             
-            module_token = strtok(line_buffer, ":");
-            topics_token = strtok(NULL, "\n");
+            module_title = strtok(line, ":");  //strtok() cuts a string into pieces using a separator.Here, the separator is :
+            module_description = strtok(NULL, "\n");
 
-            if(module_token != NULL)
+            if(module_title != NULL)
             {
-                strcpy(current_module, module_token);
-                if(topics_token != NULL)
+                strcpy(module_name, module_title);
+                if(module_description != NULL)
                 {
-                    strcpy(topic_accumulator, topics_token);
+                    strcpy(module_topics, module_description);
                 }
                 else
                 {
-                    strcpy(topic_accumulator, "");
+                    strcpy(module_topics, "");
                 }
             }
         }
         else
         {
-            if (strlen(current_module) > 0)
+            if (strlen(module_name) > 0)
             {
-                len = strlen(line_buffer);
-                if( len > 0 && line_buffer[len - 1] == '\n')
+                line_length = strlen(line);
+                if( line_length > 0 && line[line_length - 1] == '\n')
                 {
-                    line_buffer[len - 1] = '\0';
+                    line[line_length - 1] = '\0';
                 }
-                strcat(topic_accumulator, " ");
-                strcat(topic_accumulator, line_buffer);
+                strcat(module_topics, " ");
+                strcat(module_topics, line);
             }
         }
     }
-    if (strlen(current_module) > 0)
+    if (strlen(module_name) > 0)
     {
-        if(!is_first_item)
+        if(!is_first_note)
         {
             fprintf(output_file, ",\n");
         }
-        fprintf(output_file, "   \"%s\": \"%s\"", trim_spaces(current_module), trim_spaces(topic_accumulator));
+        fprintf(output_file, "   \"%s\": \"%s\"", trim_spaces(module_name), trim_spaces(module_topics));
     }
     fprintf(output_file, "\n }\n}\n");
     printf("░░░ full Syllabus Paragraph JSON generated Successfully! ░░░\n");
     
-    free(topic_accumulator);
+    free(module_topics);
 }
+
 
 
 
@@ -186,33 +289,33 @@ void parse_syllabus(FILE *input_file, FILE *output_file)
 //function to take a raw list of download links and convert them into a structured list of JSON objects
 void parse_notes(FILE *input_file, FILE *output_file)
 {
-    char line_buffer[512];
-    int is_first_item = 1;
+    char line[512];
+    int is_first_note = 1;
 
     fprintf(output_file, "{\n  \"type\": \"notes\", \n  \"materials\":[\n");
-    while (fgets(line_buffer, sizeof(line_buffer), input_file) != NULL)
+    while (fgets(line, sizeof(line), input_file) != NULL)
     {
-        if(strstr(line_buffer, "|") != NULL)
+        if(strstr(line, "|") != NULL)
         {
-            char *title_token, *author_token, *link_token;
+            char *note_title, *author_name, *download_link;
 
-            title_token = strtok(line_buffer, "|");
-            author_token = strtok(NULL, "|");
-            link_token = strtok(NULL, "\n");
+            note_title = strtok(line, "|");
+            author_name = strtok(NULL, "|");
+            download_link = strtok(NULL, "\n");
 
-            if(title_token != NULL && author_token != NULL && link_token != NULL)
+            if(note_title != NULL && author_name != NULL && download_link != NULL)
             {
-                if (!is_first_item)
+                if (!is_first_note)
                 {
                     fprintf(output_file, ",\n");
                 }
                 fprintf(output_file, "    {\n");
-                fprintf(output_file, "    \"title\": \"%s\", \n", trim_spaces(title_token));
-                fprintf(output_file, "    \"author\": \"%s\", \n", trim_spaces(author_token));
-                fprintf(output_file, "    \"link\": \"%s\"\n", trim_spaces(link_token));
+                fprintf(output_file, "    \"title\": \"%s\", \n", trim_spaces(note_title));
+                fprintf(output_file, "    \"author\": \"%s\", \n", trim_spaces(author_name));
+                fprintf(output_file, "    \"link\": \"%s\"\n", trim_spaces(download_link));
                 fprintf(output_file, "    }");
 
-                is_first_item = 0;
+                is_first_note = 0;
             }
         }
     }
